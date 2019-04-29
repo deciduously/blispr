1c9c5c48397f954b7b872633ca844d59223b915d
deciduously <ben@deciduously.com>
2019-04-28 20:09:20 -0400

    cleanup

--- src/parse.rs
+++ src/parse.rs
@@ -63,16 +63,12 @@ pub fn repl() -> Result<(), BlisprError> {
         println!("No history found.");
     }
 
-    //let mut program = String::new();
-    // let e = Rc::new(Lenv::new());
     loop {
         let input = rl.readline("blispr> ");
 
         match input {
             Ok(line) => {
                 rl.add_history_entry(line.as_ref());
-                // read last line from program? because it lives as long as the env
-
                 let parsed = match BlisprParser::parse(Rule::blispr, &line) {
                     Ok(mut iter) => iter.next().unwrap(),
                     Err(err) => {
@@ -81,10 +77,8 @@ pub fn repl() -> Result<(), BlisprError> {
                     }
                 };
                 debug!("{}", parsed);
-                let parsed_clone = parsed.clone(); // hack to get around &line prob?
-                let lval_ptr = lval_read(parsed_clone)?;
+                let lval_ptr = lval_read(parsed)?;
                 debug!("Parsed: {:?}", *lval_ptr);
-                //let env = Rc::new(Lenv::new()); // this is going to be a problem - it bootstraps on every line!
                 match lval_eval(Arc::clone(&ENV), lval_ptr) {
                     Ok(r) => {
                         println!("{}", r);
